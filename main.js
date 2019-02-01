7ea30a9f03bd199b2e9cc9faa8a0368cc95a9408
nirupath <nirushapath@gmail.com>
2019-02-01 11:52:03 +0530

    funwork

---
+++ main.js
@@ -0,0 +1,15 @@
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+  SC.stream('/tracks/498231915',function(sound){
+  $('#start').click(function(e) {
+          e.preventDefault();
+          sound.start();
+         });
+   $('#stop').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+                  });
+});
+ });
